b0a76425e3a6355735cf72d743ffcf9672e7a3d9
sabrina <34921800+soymjolk@users.noreply.github.com>
2019-01-20 21:00:17 +0000

    a couple tweaks

--- js/meadow.js
+++ js/meadow.js
@@ -7,9 +7,8 @@ function time() {
 	var s = d.getSeconds(); //seconds - unimplemented
 	var g = ["Hello, ", "Howdy, ", "Greetings, ", "Hey, ", "Yo, ", "What a nice day we're having, ", "Woah, it's "];
 	var gr = g[Math.floor(Math.random() * g.length)]; // selected greeting
-	var n = "";
 	var p = prompt("what's your name? \(leave blank for frosted tip man\)");
-	console.log("p = " + p);
+	if (!p) p="Guy Fieri";
 	document.getElementById("h1-hello").innerHTML = (gr + p + "!");
 
 	function update(d) {
